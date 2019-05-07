4f10ebc9f42d797440309e906984eddcec786146
Jkmo72 <47998782+Jkmo72@users.noreply.github.com>
2019-05-06 20:26:08 -0500

    Update topotaller.py

--- topotaller.py
+++ topotaller.py
@@ -10,14 +10,17 @@ class MyTopo( Topo ):
       Host1 = self.addHost( 'h1' )
       Host2 = self.addHost( 'h2' )
       Host3 = self.addHost( 'h3' )
+      Host4 = self.addHost( 'h4' )
       Switch1 = self.addSwitch( 's1' )
       Switch2 = self.addSwitch( 's2' )
 
         # Add links
       self.addLink( Host1, Switch1 )
+      self.addLink( Host2, Switch1 )
+      self.addLink( Host3, Switch1 )
       self.addLink( Switch1, Switch2 )
-      self.addLink( Switch2, Host2 )
-      self.addLink( Switch2, Host3 )
+      self.addLink( Switch2, Host4 )
+     
 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
